dd488a2ba9de7f87285c55a27585ebc6366abfce
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2022-10-27 09:24:10 +0000

    [Update]

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,8 +1,12 @@
 class PostImage < ApplicationRecord
   
-  has_one_attached :image
   belongs_to :user
+  has_one_attached :image
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  validates :shop_name, presence: true
+  validates :image, presence: true
   
   def get_image
     if image.attached?
@@ -20,4 +24,7 @@ class PostImage < ApplicationRecord
     image
   end
   
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy
   ]do
-  resources :post_comments, only: [:create, :destroy
-  ]
-  end
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+   end
   
   resources :users, only: [:show, :edit, :update]
 
